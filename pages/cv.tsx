859ae784c594fce24410d735366b7f5783ecafa5
akshitdandyan <akshitdandyan5287@gmail.com>
2022-12-10 09:47:28 +0530

    update cv link

--- pages/cv.tsx
+++ pages/cv.tsx
@@ -4,7 +4,7 @@ import { useLayoutEffect } from "react";
 const Cv: NextPage = () => {
     useLayoutEffect(() => {
         window.location.href =
-            "https://drive.google.com/file/d/1mEuer95CS8G6eWaS2Qbkul9kD-pcDBjj/view?usp=sharing";
+            "https://drive.google.com/file/d/1FGuepCdDyQIiT6unxTDd6sdDdV1869xM/view?usp=sharing";
     }, []);
     return <div className="p-4"></div>;
 };
